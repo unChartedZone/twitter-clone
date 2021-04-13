2f24faaabe85a3135326c9242ba27265b6695475
Christian Valdez <christianvaldez36@hotmail.com>
2021-04-12 22:23:46 -0700

    created basic textfield component

---
+++ components/VTextfield.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="relative border border-gray-400 rounded-md p-4 focus-within:border-blue-500"
+  >
+    <label class="absolute">{{ label }}</label>
+    <input
+      class="w-full bg-transparent outline-none"
+      :placeholder="placeholder"
+      :type="type"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    label: {
+      default: '',
+      type: String,
+    },
+    placeholder: {
+      default: '',
+      type: String,
+    },
+    type: {
+      default: 'text',
+      type: String,
+    },
+  },
+});
+</script>
