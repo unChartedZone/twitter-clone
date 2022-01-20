f04a5c6f7a64f37ac38d0d0c410469e17d08e87a
Christian Valdez <christianvaldez36@hotmail.com>
2022-01-20 08:39:41 -0800

    added bcrypt gem for passwords, updated user model for authentication

--- twitter-api/app/models/user.rb
+++ twitter-api/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+
+  def as_json(options = {})
+    super(except: [:password_digest])
+  end
 end
