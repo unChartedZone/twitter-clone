7ed531e7e5c47a29f07d4f87e157505858c0bbce
Christian Valdez <christianvaldez36@hotmail.com>
2022-05-11 18:51:30 -0700

    recreated tweet model

--- twitter-api/db/schema.rb
+++ twitter-api/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_073628) do
+ActiveRecord::Schema.define(version: 2022_05_12_012810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "text"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2022_02_19_073628) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tweets", "users"
 end
