880d5bd3f46a5d2d9b98abd4ce238b7d7a943db9
Christian Valdez <christianvaldez36@hotmail.com>
2022-05-11 18:22:44 -0700

    switched api to use postgres instead of sqlite

--- twitter-api/db/schema.rb
+++ twitter-api/db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_070012) do
+ActiveRecord::Schema.define(version: 2022_02_19_073628) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "username"
