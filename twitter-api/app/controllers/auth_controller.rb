23e8d9c7d53d1b670789d4972cd828ef4353a3f5
Christian Valdez <christianvaldez36@hotmail.com>
2022-05-11 21:49:50 -0700

    refactored alot of jwt code to lib class

--- twitter-api/app/controllers/auth_controller.rb
+++ twitter-api/app/controllers/auth_controller.rb
@@ -1,19 +1,16 @@
 class AuthController < ApplicationController
   require 'jwt'
 
-  before_action :setup_env
-
-  def setup_env
-    @access_secret = ENV['ACCESS_SECRET']
-    # @refresh_secret = Rails.configuration.x.refresh_secret
-    @refresh_secret = ENV['REFRESH_SECRET']
-  end
-
   def signup
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      access_token = JsonWebToken.generate_access_token(@user)
+      refresh_token = JsonWebToken.generate_refresh_token(@user)
+
+      set_refresh_token_cookie(refresh_token)
+
+      render json: {access_token: access_token, user: @user}, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -22,60 +19,44 @@ class AuthController < ApplicationController
   def login
     @user = User.find_by(email: login_params[:email])
 
-    # User not registered, maybe this could be a security vulnerability?
-    unless @user
-      return render json: { message: 'Username not registered.' }, status: :unauthorized
-    end
-
-    # If passwords don't match then auth fails
     unless @user.authenticate(login_params[:password])
       return render json: { message: 'Username or Password was incorrect.' }, status: :unauthorized
     end
 
-    access_payload = { user_id: @user.id, email: @user.email, exp: Time.now.to_i + 15 * 60 }
-    refresh_payload = { user_id: @user.id, email: @user.email, exp: Time.now.to_i + 60 * 60 * 24 }
+    access_token = JsonWebToken.generate_access_token(@user)
+    refresh_token = JsonWebToken.generate_refresh_token(@user)
 
-    access_token = generate_access_jwt(access_payload)
-    refresh_token = generate_refresh_token(refresh_payload)
-    # set_refresh_token_cookie(refresh_token)
-    cookies[:refresh_token] = {
-      value: refresh_token,
-      httponly: true,
-      secure: true,
-      expires: 7.days.from_now
-    }
+    set_refresh_token_cookie(refresh_token)
 
     render json: { access_token: access_token, user: @user }
   end
 
   def refresh_current_token
+    # Grab refresh token from request cookies
     refresh_token = cookies[:refresh_token]
 
+    # If there isn't a refresh token cookie, then send back unauthorized response
     unless refresh_token
       return render json: { access_token: '' }, status: :unauthorized
     end
 
     begin
-      decoded_refresh_token = JWT.decode(refresh_token, @refresh_secret, true)
+      decoded_refresh_token = JsonWebToken.decode_refresh_token(refresh_token)
     rescue JWT::DecodeError
-      Rails.logger.warn  'Error decoding refresh jwt'
+      Rails.logger.warn 'Error decoding refresh jwt'
       set_refresh_token_cookie('') # set an empty refresh token in the cookie
-      return render json: {message: 'SUPER NOT AUTHORIZED', access_token: ''}, status: :unauthorized
+      return render json: { message: 'SUPER NOT AUTHORIZED', access_token: '' }, status: :unauthorized
     end
 
-    payload = decoded_refresh_token[0]
-    user_id = payload['user_id']
+    user_id = decoded_refresh_token['user_id']
     @user = User.find(user_id)
 
-    access_payload = { user_id: @user.id, email: @user.email, exp: Time.now.to_i + 15 * 60 }
-    refresh_payload = { user_id: @user.id, email: @user.email, exp: Time.now.to_i + 60 * 60 * 24 }
+    access_token = JsonWebToken.generate_access_token(@user)
+    refresh_token = JsonWebToken.generate_refresh_token(@user)
 
-    new_access_token = generate_access_jwt(access_payload)
-    new_refresh_token = generate_refresh_token(refresh_payload)
+    set_refresh_token_cookie(refresh_token)
 
-    set_refresh_token_cookie(new_refresh_token)
-
-    render json: { access_token: new_access_token, user: @user, refresh_token: new_refresh_token, message: 'Refreshed Token!' }
+    render json: { access_token: access_token, user: @user, message: 'Refreshed Token!' }
   end
 
   def user
@@ -101,14 +82,6 @@ class AuthController < ApplicationController
     user = User.find(user_id)
 
     render json: { user: user, message: 'Authorized' }
-  end
-
-  def generate_access_jwt(payload)
-    JWT.encode(payload, @access_secret, 'HS256')
-  end
-
-  def generate_refresh_token(payload)
-    JWT.encode(payload, @refresh_secret, 'HS256')
   end
 
   def set_refresh_token_cookie(token)
