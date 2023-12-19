423cedf2e531a5fa8a59f9c43689971e45301abd
Christian Valdez <christianvaldez36@hotmail.com>
2023-12-19 10:12:02 -0800

    added types file for components

---
+++ twitter-ui/src/types/components.d.ts
@@ -0,0 +1,27 @@
+import Button from "@/components/common/Button.vue";
+import Card from "@/components/common/Card.vue";
+import Icon from "@/components/common/Icon.vue";
+import Link from "@/components/common/Link.vue";
+import List from "@/components/common/List.vue";
+import ListItem from "@/components/common/ListItem.vue";
+import Menu from "@/components/common/Menu.vue";
+import Modal from "@/components/common/Modal.vue";
+import TabHeader from "@/components/common/TabHeader.vue";
+import Tabs from "@/components/common/Tabs.vue";
+import Textfield from "@/components/common/Textfield.vue";
+
+declare module "@vue/runtime-core" {
+  export interface GlobalComponents {
+    Button: typeof Button;
+    Card: typeof Card;
+    Icon: typeof Icon;
+    Link: typeof Link;
+    List: typeof List;
+    ListItem: typeof ListItem;
+    Menu: typeof Menu;
+    Modal: typeof Modal;
+    Tabs: typeof Tabs;
+    Textfield: typeof Textfield;
+    TabHeader: typeof TabHeader;
+  }
+}
