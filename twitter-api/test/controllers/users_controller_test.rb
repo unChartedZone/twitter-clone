68539c51bcb1e31065753666f2595b0e49cf470d
Christian Valdez <christianvaldez36@hotmail.com>
2024-01-11 21:23:22 -0800

    added tests for user controller

---
+++ twitter-api/test/controllers/users_controller_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
+
+  test "should get users" do
+    get users_url, as: :json
+    assert_response :success
+    json_response = JSON.parse(self.response.body, symbolize_names: true)
+    assert_equal users.size, json_response.size
+  end
+
+  test "should get user" do
+    get user_url(@user), as: :json
+    assert_response :success
+    json_response = JSON.parse(self.response.body, symbolize_names: true)
+    assert_equal @user.email, json_response[:email]
+    assert_equal @user.username, json_response[:username]
+  end
+
+  test "should update user" do
+    patch user_url(@user), params: { name: "Another name" }, headers: { Authorization: "Bearer #{JsonWebToken.generate_access_token(@user)}" }, as: :json
+    assert_response :success
+    json_response = JSON.parse(self.response.body, symbolize_names: true)
+    assert_equal "Another name", json_response[:name]
+  end
+
+  test "should not update user if not the owner" do
+    patch user_url(users(:two)), params: { name: "New name" }, headers: { Authorization: "Bearer #{JsonWebToken.generate_access_token(@user)}" }, as: :json
+    assert_response :forbidden
+  end
+
+  test "should get current self" do
+    get me_url, headers: { Authorization: "Bearer #{JsonWebToken.generate_access_token(@user)}" }, as: :json
+    assert_response :success
+    json_response = JSON.parse(self.response.body, symbolize_names: true)
+    assert_equal @user.email, json_response.dig(:data, :attributes, :email)
+  end
+
+  test "should not get current self if not authenticated" do
+    get me_url, as: :json
+    assert_response :unauthorized
+  end
+
+  # TODO: implement these tests
+  test "update profile image" do
+  end
+  test "update banner image" do
+  end
+end
