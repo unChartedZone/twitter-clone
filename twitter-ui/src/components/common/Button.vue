4430c9a8d5aa0dabae65f1d39c569874f740fafb
Christian Valdez <christianvaldez36@hotmail.com>
2024-01-12 18:40:00 -0800

    updated button component to use icon component within it

--- twitter-ui/src/components/common/Button.vue
+++ twitter-ui/src/components/common/Button.vue
@@ -1,24 +1,35 @@
 <script setup lang="ts">
 import { defineProps } from "vue";
+import type { IconName } from "./Icon.vue";
 
 interface ButtonProps {
   block?: boolean;
-  icon?: boolean;
+  icon?: IconName;
   outline?: boolean;
-  color?: string;
+  color?: "primary" | "secondary" | string;
+  tonal?: boolean;
   type?: "button" | "submit" | "reset";
 }
 
-withDefaults(defineProps<ButtonProps>(), { color: "primary", type: "button" });
+withDefaults(defineProps<ButtonProps>(), {
+  color: "primary",
+  type: "button",
+});
 </script>
 
 <template>
   <button
     class="btn"
-    :class="{ 'btn--block': block, 'btn--icon': icon, 'btn--outline': outline }"
+    :class="{
+      'btn--block': block,
+      'btn--icon': icon,
+      'btn--outline': outline,
+      'btn--tonal': tonal,
+    }"
     :type="type"
   >
-    <slot />
+    <Icon v-if="!!icon" :name="icon" :fill="tonal ? 'white' : 'black'" />
+    <slot v-else />
   </button>
 </template>
 
@@ -64,6 +75,10 @@ withDefaults(defineProps<ButtonProps>(), { color: "primary", type: "button" });
       // TODO: make this color a variable
       background-color: rgba(15, 20, 25, 0.1);
     }
+  }
+
+  &--tonal {
+    background-color: rgba($black, 0.6);
   }
 }
 </style>
