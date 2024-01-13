b9fbc32c67f470b392a6341b7743ad30b0ef73ff
Christian Valdez <christianvaldez36@hotmail.com>
2024-01-12 19:46:28 -0800

    updated layout of index view page

--- twitter-ui/src/views/IndexView.vue
+++ twitter-ui/src/views/IndexView.vue
@@ -5,96 +5,106 @@ const year = computed(() => new Date().getFullYear());
 </script>
 
 <template>
-  <main>
-    <div class="main-section">
+  <main class="page">
+    <div class="page__content">
       <section class="icon-section">
-        <Icon name="bird" />
+        <Icon name="bird" :size="40" fill="white" />
       </section>
       <section class="action-section">
-        <Icon name="bird" />
-        <h1>Happening Now</h1>
+        <Icon name="bird" size="md" />
+        <h1>Happening now</h1>
         <h2>Join Twitter Today.</h2>
-        <div class="login-flow-options">
-          <Link to="/signup">Sign up</Link>
-          <Link to="/login" outline>Log in</Link>
+        <div class="action-section__links space-y-4">
+          <Button block>Sign up</Button>
+          <Link to="/login" block outline>Log in</Link>
         </div>
       </section>
     </div>
-    <section>
-      <footer class="footer">
-        <ul>
-          <li>About</li>
-          <li>Help Center</li>
-          <li>Terms of Service</li>
-          <li>Privacy Policy</li>
-          <li>Cookie Policy</li>
-          <li>Ads Info</li>
-          <li>Blog</li>
-          <li>Status</li>
-          <li>Careers</li>
-          <li>Brand Resources</li>
-          <li>Advertising</li>
-          <li>Marketing</li>
-          <li>Twitter for Business</li>
-          <li>Developers</li>
-          <li>Directory</li>
-          <li>Settings</li>
-          <li>@{{ year }} Twitter, Inc.</li>
-        </ul>
-      </footer>
-    </section>
+    <footer class="page__footer">
+      <ul>
+        <li>About</li>
+        <li>Help Center</li>
+        <li>Terms of Service</li>
+        <li>Privacy Policy</li>
+        <li>Cookie Policy</li>
+        <li>Ads Info</li>
+        <li>Blog</li>
+        <li>Status</li>
+        <li>Careers</li>
+        <li>Brand Resources</li>
+        <li>Advertising</li>
+        <li>Marketing</li>
+        <li>Twitter for Business</li>
+        <li>Developers</li>
+        <li>Directory</li>
+        <li>Settings</li>
+        <li>@{{ year }} Twitter, Inc.</li>
+      </ul>
+    </footer>
   </main>
 </template>
 
 <style scoped lang="scss">
-.main-section {
-  display: grid;
-  grid-template-columns: repeat(2, minmax(0, 1fr));
-  height: 90vh;
+.page {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 
-  section {
-    height: 100%;
+  &__content {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+    flex-grow: 1;
+    align-items: center;
+    justify-items: center;
+  }
+
+  &__footer {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 1rem 0;
+
+    ul {
+      display: flex;
+      justify-content: center;
+      flex-wrap: 1;
+      gap: 0.5rem;
+    }
+
+    li {
+      list-style: none;
+    }
   }
 }
 
 .icon-section {
-  height: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+  height: 100%;
+  width: 100%;
 }
 
 .action-section {
   svg {
-    height: 4rem;
+    margin-bottom: 3rem;
   }
 
   h1 {
     font-size: 3.75rem;
-    font-weight: 700;
+    font-weight: 800;
     line-height: 1;
+    margin-bottom: 3rem;
   }
 
   h2 {
     font-size: 1.875rem;
-  }
-}
-
-.login-flow-options {
-  width: 24rem;
-}
-
-.footer {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-
-  ul {
-    display: flex;
-    justify-content: center;
-    flex-wrap: 1;
-    gap: 0.5rem;
+    margin-bottom: 1rem;
   }
 
-  li {
-    list-style: none;
+  &__links {
+    width: 20rem;
   }
 }
 </style>
