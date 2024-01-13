2a7ef7fe5ab351438933596d3bd1c2dbc1a07285
Christian Valdez <christianvaldez36@hotmail.com>
2024-01-12 19:45:20 -0800

    added some generated styles for spacing

--- twitter-ui/src/views/HomeView.vue
+++ twitter-ui/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="home-page">
     <section>Home Page</section>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.home-page {
+  height: 110vh;
+}
+</style>
