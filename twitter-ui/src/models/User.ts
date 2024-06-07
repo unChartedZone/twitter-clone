1d89ba407d970ee4ef1d7eb8ea9ad5bb25ede21e
Christian Valdez <christianvaldez36@hotmail.com>
2024-06-06 20:32:03 -0700

    updated user model

--- twitter-ui/src/models/User.ts
+++ twitter-ui/src/models/User.ts
@@ -1,12 +1,25 @@
 export interface BaseUser {
+  id: string;
   email: string;
   username: string;
   name: string;
-  profileImage: string;
-  bannerImage: string;
+  bio?: string;
+  dateOfBirth?: string;
+  location?: string;
+  website?: string;
+  profileImage?: string;
+  bannerImage?: string;
+}
+
+// User fields that are typically modified
+export interface UserPatch {
+  name?: string;
+  bio?: string;
+  dateOfBirth?: string;
+  location?: string;
+  website?: string;
 }
 
 export default interface User extends BaseUser {
-  id: string;
   accessToken: string;
 }
