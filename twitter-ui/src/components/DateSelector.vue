010d9a23a0c3373fd91395500148f30c39f3fa1b
Christian Valdez <christianvaldez36@hotmail.com>
2024-07-03 00:22:16 -0700

    created date selector component

---
+++ twitter-ui/src/components/DateSelector.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="date-selector">
+    <Select v-model="dateState.month" label="Month" :options="months" />
+    <Select v-model="dateState.day" label="Day" :options="days" />
+    <Select v-model="dateState.year" label="Year" :options="years" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, computed, watch } from "vue";
+import dayjs from "dayjs";
+
+interface DateSelectorProps {
+  modelValue: Date;
+}
+
+const props = defineProps<DateSelectorProps>();
+const emit = defineEmits(["update:modelValue"]);
+
+const date = dayjs(props.modelValue).toDate();
+const dateState = reactive<{
+  month: string;
+  day?: number;
+  year: number;
+}>({
+  month: date.toLocaleString("default", { month: "long" }),
+  day: dayjs(date).date(),
+  year: dayjs(date).year(),
+});
+
+const months = Array.from({ length: 12 }, (_item, i) =>
+  new Date(0, i).toLocaleDateString("en-US", { month: "long" })
+);
+
+const days = computed(() => {
+  const daysInMonth = dayjs(
+    `${dateState.year}-${dateState.month}-${dateState.day}`
+  ).daysInMonth();
+  return Array.from({ length: daysInMonth }, (_, index) => index + 1);
+});
+
+const years = Array.from(
+  { length: new Date().getFullYear() - 1900 },
+  (_, index) => 1901 + index
+).reverse();
+
+watch(
+  () => dateState.month,
+  (newVal, oldVal) => {
+    if (dateState.day && dateState.day > days.value.length) {
+      dateState.day = undefined;
+    }
+  }
+);
+
+watch(dateState, (newDate, oldDate) => {
+  emit(
+    "update:modelValue",
+    dayjs(`${newDate.year}-${newDate.month}-${newDate.day}`).toDate()
+  );
+});
+</script>
+
+<style lang="scss">
+.date-selector {
+  display: flex;
+  gap: 1rem;
+}
+</style>
