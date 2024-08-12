2103899a04c9c1ac9d8157ec64c0c65ebcb6d27c
Christian Valdez <christianvaldez36@hotmail.com>
2024-08-12 09:58:07 -0700

    added production url to cors config

--- twitter-api/config/initializers/cors.rb
+++ twitter-api/config/initializers/cors.rb
@@ -16,7 +16,7 @@
 # end
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080'
+    origins 'http://localhost:8080', 'https://twitter.chrisvaldez.dev'
 
     resource '*',
              headers: :any,
