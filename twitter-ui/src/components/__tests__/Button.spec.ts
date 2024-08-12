32533970f7a591209da2a6c48257e55839089d9d
Christian Valdez <christianvaldez36@hotmail.com>
2024-08-11 18:43:47 -0700

    fix button test

--- twitter-ui/src/components/__tests__/Button.spec.ts
+++ twitter-ui/src/components/__tests__/Button.spec.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { mount } from "@vue/test-utils";
-import Button from "../Button.vue";
+import Button from "../common/Button.vue";
 
 describe("Button", () => {
   it("renders properly", () => {
