ca7e19a639a73010434692df9f2460ab50365bbc
Christian Valdez <christianvaldez36@hotmail.com>
2024-11-27 18:00:23 -0800

    added some more logging D:

--- twitter-api/app/controllers/auth_controller.rb
+++ twitter-api/app/controllers/auth_controller.rb
@@ -1,18 +1,20 @@
 class AuthController < ApplicationController
   require 'jwt'
   DOMAIN = Rails.application.credentials.domain.to_s
+  ACCESS_SECRET = Rails.application.credentials.access_secret.to_s
+  REFRESH_SECRET = Rails.application.credentials.refresh_secret.to_s
 
   def signup
     @user = User.create(user_params)
 
-    if not @user.save
+    if !@user.save
       render json: @user.errors, status: :unprocessable_entity
     else
       access_token = JsonWebToken.generate_access_token(@user)
       refresh_token = JsonWebToken.generate_refresh_token(@user)
       set_refresh_token_cookie(refresh_token)
       Rails.logger.info 'Signing up user #{@user.username}'
-      render json: UserSerializer.new(@user, { params: { access_token: access_token } }), status: :created
+      render json: UserSerializer.new(@user, { params: { access_token: } }), status: :created
     end
   end
 
@@ -26,7 +28,7 @@ class AuthController < ApplicationController
     refresh_token = JsonWebToken.generate_refresh_token(@user)
     set_refresh_token_cookie(refresh_token)
     Rails.logger.info "Logging in user #{@user.username}"
-    render json: UserSerializer.new(@user, { params: { access_token: access_token } })
+    render json: UserSerializer.new(@user, { params: { access_token: } })
   end
 
   def refresh_current_token
@@ -36,9 +38,7 @@ class AuthController < ApplicationController
 
     Rails.logger.info("Received following refresh token #{refresh_token}")
     # If there isn't a refresh token cookie, then send back unauthorized response
-    unless refresh_token
-      return render json: { access_token: '' }, status: :unauthorized
-    end
+    return render json: { access_token: '' }, status: :unauthorized unless refresh_token
 
     begin
       decoded_refresh_token = JsonWebToken.decode_refresh_token(refresh_token)
@@ -54,9 +54,13 @@ class AuthController < ApplicationController
     access_token = JsonWebToken.generate_access_token(@user)
     refresh_token = JsonWebToken.generate_refresh_token(@user)
 
+    Rails.logger.info("Refreshing, created access token: #{access_token}")
+    Rails.logger.info("DOMAIN: #{DOMAIN}")
+    Rails.logger.info("Access Secret: #{ACCESS_SECRET}")
+
     set_refresh_token_cookie(refresh_token)
 
-    render json: UserSerializer.new(@user, { params: { access_token: access_token } })
+    render json: UserSerializer.new(@user, { params: { access_token: } })
   end
 
   def logout
