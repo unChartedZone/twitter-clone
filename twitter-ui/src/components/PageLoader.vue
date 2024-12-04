105e20273991977155f3c95d216327a8e0a83d83
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-04 10:57:16 -0800

    created page loader component

---
+++ twitter-ui/src/components/PageLoader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import LoadingIcon from "./common/LoadingIcon.vue";
+</script>
+
+<template>
+  <div class="page-loader">
+    <LoadingIcon />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.page-loader {
+  display: flex;
+  justify-content: center;
+  margin: 1rem 0;
+}
+</style>
