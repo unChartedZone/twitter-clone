ac1aacc61dc220f86e9a417219630a88c4bc80a0
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-06 20:11:32 -0800

    created new icon file to hopefully move over all icons to

---
+++ twitter-ui/src/components/icons/Icon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { Component } from "vue";
+import Bird from "./Bird.vue";
+import Feather from "./Feather.vue";
+
+type IconVariant = "bird" | "feather";
+
+defineProps<{ variant: IconVariant }>();
+
+const iconComponentMap = new Map<IconVariant, Component>();
+iconComponentMap.set("bird", Bird);
+iconComponentMap.set("feather", Feather);
+</script>
+
+<template>
+  <component class="icon" :is="iconComponentMap.get(variant)" />
+</template>
+
+<style scoped lang="scss">
+.icon {
+  min-height: 1rem;
+}
+</style>
