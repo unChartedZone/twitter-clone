7f1ad3caea33917bec9f4d1cc684d470167d814f
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-11 22:14:15 -0800

    added more icons to new component

--- twitter-ui/src/components/icons/Icon.vue
+++ twitter-ui/src/components/icons/Icon.vue
@@ -1,15 +1,41 @@
 <script setup lang="ts">
 import type { Component } from "vue";
 import Bird from "./Bird.vue";
+import Bookmark from "./Bookmark.vue";
+import BookmarkOutline from "./BookmarkOutline.vue";
+import Ellipsis from "./Ellipsis.vue";
 import Feather from "./Feather.vue";
+import Heart from "./Heart.vue";
+import HeartOutline from "./HeartOutline.vue";
+import Reply from "./Reply.vue";
+import Retweet from "./Retweet.vue";
+import Share from "./Share.vue";
 
-type IconVariant = "bird" | "feather";
+export type IconVariant =
+  | "bird"
+  | "bookmark"
+  | "bookmark-outline"
+  | "ellipsis"
+  | "feather"
+  | "heart"
+  | "heart-outline"
+  | "reply"
+  | "retweet"
+  | "share";
 
 defineProps<{ variant: IconVariant }>();
 
 const iconComponentMap = new Map<IconVariant, Component>();
 iconComponentMap.set("bird", Bird);
+iconComponentMap.set("bookmark", Bookmark);
+iconComponentMap.set("bookmark-outline", BookmarkOutline);
 iconComponentMap.set("feather", Feather);
+iconComponentMap.set("ellipsis", Ellipsis);
+iconComponentMap.set("heart", Heart);
+iconComponentMap.set("heart-outline", HeartOutline);
+iconComponentMap.set("reply", Reply);
+iconComponentMap.set("retweet", Retweet);
+iconComponentMap.set("share", Share);
 </script>
 
 <template>
