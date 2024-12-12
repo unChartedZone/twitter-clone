9b0a2d6e79633c1cc34c412709e1e96a55e220b6
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-11 22:58:41 -0800

    made follow button use new button component

--- twitter-ui/src/components/profile/FollowButton.vue
+++ twitter-ui/src/components/profile/FollowButton.vue
@@ -1,23 +1,39 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { followUser } from "@/api/endpoints";
 import type { BaseUser } from "@/models/User";
+import Button from "../ui/Button.vue";
+import type { LoadingState } from "@/types/LoadingState";
 
 interface FollowButtonProps {
   userId: string;
 }
 
+const loading = ref<LoadingState>();
 const props = defineProps<FollowButtonProps>();
 const emit = defineEmits<{
   (e: "onFollow", followedUser: BaseUser): Promise<void>;
 }>();
 
 async function follow() {
-  const res = await followUser(props.userId);
-  const followedUser = res.followed_user;
-  emit("onFollow", followedUser);
+  loading.value = "idle";
+  try {
+    const res = await followUser(props.userId);
+    const followedUser = res.followed_user;
+    emit("onFollow", followedUser);
+    loading.value = "resolved";
+  } catch (e) {
+    loading.value = "rejected";
+  }
 }
 </script>
 
 <template>
-  <Button color="black" :size="1" @click.stop.prevent="follow">Follow</Button>
+  <Button
+    :loading="loading === 'idle'"
+    variant="monochrome"
+    @click.stop.prevent="follow"
+  >
+    Follow
+  </Button>
 </template>
