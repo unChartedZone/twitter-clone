ba4406a09a0612e0cd700ce126959f407ee98a3b
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-11 22:18:27 -0800

    improved new button to have diffferent sizes

--- twitter-ui/src/components/ui/Button.vue
+++ twitter-ui/src/components/ui/Button.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 // TODO: use this button replace other button
 interface ButtonProps {
-  variant?: "primary" | "outline" | "destructive" | "icon";
+  variant?:
+    | "primary"
+    | "outline"
+    | "destructive"
+    | "icon"
+    | "icon-ghost";
   type?: "button" | "submit" | "reset";
-  size?: "xs" | "sm" | "default" | "lg" | "xl" | "icon";
+  size?: "xs" | "sm" | "default" | "lg" | "xl" | "icon" | "icon-sm";
 }
 
 withDefaults(defineProps<ButtonProps>(), {
@@ -14,28 +19,38 @@ withDefaults(defineProps<ButtonProps>(), {
 </script>
 
 <template>
-  <button :type="type" class="btn" :class="[`btn--${variant}`]">
+  <button
+    :type="type"
+    class="btn"
+    :class="[`btn--${variant}`, `btn--size-${size}`]"
+  >
     <slot />
   </button>
 </template>
 
 <style scoped lang="scss">
 .btn {
+  border: none;
+  outline: none;
   cursor: pointer;
   font-size: 1rem;
+  font-weight: 700;
   line-height: 1rem;
 
   &--primary {
-    outline: none;
-    border: none;
     border-radius: 1.5rem;
     background-color: $primary;
     color: $white;
     position: relative;
-    font-weight: 700;
     min-width: 2rem;
-    padding: 0.5rem;
+    padding: 0.5rem 1rem;
     width: 100%;
+
+    &:hover {
+      background-color: lighten($primary, 0.4);
+    }
+  }
+
   }
 
   &--outline {
@@ -47,7 +62,6 @@ withDefaults(defineProps<ButtonProps>(), {
   }
 
   &--icon {
-    border: none;
     background-color: $black;
     height: 40px;
     width: 40px;
@@ -59,6 +73,36 @@ withDefaults(defineProps<ButtonProps>(), {
     &:hover {
       background-color: adjust-color($color: $black, $lightness: 20%);
     }
+  }
+
+  &--icon-ghost {
+    background-color: transparent;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+      background-color: adjust-color(
+        $color: $primary,
+        $lightness: 40%,
+        $alpha: 0.7
+      );
+      fill: $primary;
+    }
+  }
+
+  &--size-icon {
+    height: 35px;
+    width: 35px;
+    padding: 0.5rem;
+  }
+
+  &--size-icon-sm {
+    height: 30px;
+    width: 30px;
+    padding: 0.45rem;
   }
 }
 </style>
