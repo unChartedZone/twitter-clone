69db7da89f1e0f2c99871b2557f3ae80c8aea414
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-11 22:57:10 -0800

    added loading to new button component

--- twitter-ui/src/components/ui/Button.vue
+++ twitter-ui/src/components/ui/Button.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
+import LoadingIcon from "../common/LoadingIcon.vue";
+
 // TODO: use this button replace other button
 interface ButtonProps {
   variant?:
     | "primary"
     | "outline"
+    | "monochrome"
     | "destructive"
     | "icon"
     | "icon-ghost";
   type?: "button" | "submit" | "reset";
   size?: "xs" | "sm" | "default" | "lg" | "xl" | "icon" | "icon-sm";
+  loading?: boolean;
 }
 
 withDefaults(defineProps<ButtonProps>(), {
@@ -24,26 +28,48 @@ withDefaults(defineProps<ButtonProps>(), {
     class="btn"
     :class="[`btn--${variant}`, `btn--size-${size}`]"
   >
-    <slot />
+    <div v-if="loading" class="btn__loader">
+      <LoadingIcon :size="25" />
+    </div>
+    <div class="btn__content" :class="[loading && 'loading']">
+      <slot />
+    </div>
   </button>
 </template>
 
 <style scoped lang="scss">
+$padding: 0.5rem 1rem;
+$rounded: 1.5rem;
+
 .btn {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  grid-template-areas: "stack";
   border: none;
   outline: none;
   cursor: pointer;
   font-size: 1rem;
   font-weight: 700;
   line-height: 1rem;
+  transition: all 0.3s ease-in-out;
+
+  &__content,
+  &__loader {
+    grid-area: stack;
+  }
+
+  .loading {
+    visibility: hidden;
+  }
 
   &--primary {
-    border-radius: 1.5rem;
+    border-radius: $rounded;
     background-color: $primary;
     color: $white;
     position: relative;
     min-width: 2rem;
-    padding: 0.5rem 1rem;
+    padding: $padding;
     width: 100%;
 
     &:hover {
@@ -51,6 +77,15 @@ withDefaults(defineProps<ButtonProps>(), {
     }
   }
 
+  &--monochrome {
+    border-radius: $rounded;
+    color: $white;
+    background-color: $black;
+    padding: $padding;
+
+    &:hover {
+      background-color: adjust-color($color: $black, $alpha: -0.2);
+    }
   }
 
   &--outline {
