83b747eddc9363b3ef3e152566204c4abc88153c
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-13 00:24:34 -0800

    creatd initial reply editor

---
+++ twitter-ui/src/components/ReplyEditor.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useAuthStore } from "@/stores/auth";
+import AvatarCircle from "./AvatarCircle.vue";
+import Button from "./ui/Button.vue";
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <section class="reply-editor flex items-center gap-4 py-4">
+    <AvatarCircle :src="authStore.user?.profileImage" />
+    <!-- <Textfield label="Tweet your reply" /> -->
+    <div>
+      <Button>Post</Button>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss"></style>
