065b776f5308a69f98873662b3ffd84b459a842a
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-20 13:49:25 -0800

    omg finally got menu to reactively show it's content depending if it's partially clipped or not :D 🎉

--- twitter-ui/src/components/common/Menu.vue
+++ twitter-ui/src/components/common/Menu.vue
@@ -1,20 +1,56 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
+
 interface MenuProps {
   modelValue?: boolean;
 }
 
 const props = defineProps<MenuProps>();
 const emit = defineEmits(["update:modelValue"]);
+const target = ref<HTMLElement | null>(null);
+const displaceContent = ref<boolean>(false);
 
 function handleChange() {
+  displaceContent.value = false;
   emit("update:modelValue", !props.modelValue);
 }
+
+watch(
+  () => [props.modelValue, target.value],
+  (val, _oldVal) => {
+    if (!val) return;
+
+    if (!!target.value) {
+      const rect = target.value.getBoundingClientRect();
+
+      const calculation =
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <=
+          (window.innerWidth || document.documentElement.clientWidth);
+
+      if (!calculation) {
+        displaceContent.value = true;
+      }
+    }
+  }
+);
 </script>
 
 <template>
   <div class="menu">
-    <slot name="activator" :onClick="handleChange" />
-    <div class="menu__content" @click="handleChange" v-if="modelValue">
+    <div>
+      <slot name="activator" :onClick="handleChange" />
+    </div>
+    <div
+      v-if="modelValue"
+      ref="target"
+      class="menu__content"
+      :class="{ reverse: displaceContent }"
+      @click="handleChange"
+    >
       <slot />
     </div>
   </div>
@@ -29,6 +65,11 @@ function handleChange() {
     position: absolute;
     top: -180%;
     z-index: 200;
+  }
+
+  .reverse {
+    top: 50%;
+    left: 0;
   }
 
   &__bg {
