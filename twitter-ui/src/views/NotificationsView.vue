de2dfdd838c0698b61d108c2cb555d04739e1b1d
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-23 14:29:16 -0800

    built out notification view to show notifications

--- twitter-ui/src/views/NotificationsView.vue
+++ twitter-ui/src/views/NotificationsView.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+import * as notificationApi from "@/api/endpoints/notifications";
+import type Notification from "@/models/Notification";
+import { useQuery } from "@/hooks/useQuery";
+import PageHeader from "@/components/PageHeader.vue";
+import PageLoader from "@/components/PageLoader.vue";
+import NotificationCard from "@/components/notifications/NotificationCard.vue";
+
+const { result: notifications, loading } = useQuery<Notification[]>(
+  notificationApi.fetchNotifications,
+  { initialValue: [] }
+);
+</script>
+
 <template>
-  <div>
-    <h1>Notifications Page</h1>
+  <PageHeader title="Notifications" />
+  <PageLoader v-if="loading" />
+  <div class="list">
+    <NotificationCard
+      v-for="notification in notifications"
+      :key="notification.id"
+      :notification="notification"
+    />
   </div>
 </template>
+
+<style scoped lang="scss">
+.list {
+  border-top: 1px solid $gray;
+}
+</style>
