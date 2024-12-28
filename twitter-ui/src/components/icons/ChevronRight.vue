36b1ccee5f28f591e7e10d13636d315490615547
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-28 00:39:35 -0800

    added some more icons

---
+++ twitter-ui/src/components/icons/ChevronRight.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg viewBox="0 0 24 24" aria-hidden="true">
+    <g>
+      <path
+        d="M14.586 12L7.543 4.96l1.414-1.42L17.414 12l-8.457 8.46-1.414-1.42L14.586 12z"
+      ></path>
+    </g>
+  </svg>
+</template>
