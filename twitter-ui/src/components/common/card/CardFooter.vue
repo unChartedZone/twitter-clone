dc41cd0983a9df9c7147eb2031377eb56209bcb7
Christian Valdez <christianvaldez36@hotmail.com>
2024-12-31 15:26:51 -0800

    created CardFooter component

---
+++ twitter-ui/src/components/common/card/CardFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card-footer">
+    <div>
+      <slot name="left" />
+    </div>
+    <div class="default">
+      <slot />
+    </div>
+    <div>
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card-footer {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 0.5rem;
+}
+</style>
