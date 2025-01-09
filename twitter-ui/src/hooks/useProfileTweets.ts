1723fcd27977a26bbbc8d28bf72fb53f98334dde
Christian Valdez <christianvaldez36@hotmail.com>
2025-01-09 13:34:17 -0800

    added throttling to hook that fetches tweets for profile page

--- twitter-ui/src/hooks/useProfileTweets.ts
+++ twitter-ui/src/hooks/useProfileTweets.ts
@@ -1,6 +1,6 @@
 import { onMounted, watch } from "vue";
 import { useRoute } from "vue-router";
-import { useDebounceFn } from "@vueuse/core";
+import { useThrottleFn } from "@vueuse/core";
 import type { TweetListSegment } from "@/types/TweetList";
 import { useProfileStore } from "@/stores/profile";
 import { useIsBottom } from "@/hooks/useIsBottom";
@@ -14,6 +14,15 @@ export function useProfileTweets(segment: TweetListSegment) {
   const profileStore = useProfileStore();
   const { isBottom } = useIsBottom();
 
+  const throttledTweetFetch = useThrottleFn(
+    async () =>
+      await profileStore.loadTweets(
+        route.params.username[0],
+        profileStore.tweetLists[segment].page,
+        segment
+      )
+  );
+
   onMounted(async () => {
     if (profileStore.tweetLists[segment].tweets.length > 0) return;
 
@@ -23,11 +32,7 @@ export function useProfileTweets(segment: TweetListSegment) {
   // For when a user scrolls to the bottom of a page
   watch(isBottom, async (val, oldVal) => {
     if (val && profileStore.tweetLists[segment].hasMore) {
-      await profileStore.loadTweets(
-        route.params.username[0],
-        profileStore.tweetLists[segment].page,
-        segment
-      );
+      throttledTweetFetch();
     }
   });
 }
