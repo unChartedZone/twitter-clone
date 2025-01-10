6010492bba68bb5b430fed599b32d79cbfbf0ac2
Christian Valdez <christianvaldez36@hotmail.com>
2025-01-10 14:45:20 -0800

    created hook to check if window size has reached screen size breakpoints

---
+++ twitter-ui/src/hooks/useScreenSize.ts
@@ -0,0 +1,29 @@
+import { ref, computed, watch, onMounted } from "vue";
+import { useWindowSize } from "@vueuse/core";
+
+export function useScreenSize() {
+  const { width } = useWindowSize();
+  const SM_SCREEN_SIZE = 639;
+  const MD_SCREEN_SIZE = 639;
+
+  const isSmallScreen = ref<boolean>(false);
+
+  onMounted(() => {
+    if (width.value <= SM_SCREEN_SIZE) {
+      isSmallScreen.value = true;
+    }
+  });
+
+  watch(width, (val) => {
+    console.log("Val: ", val);
+    if (val <= SM_SCREEN_SIZE) {
+      isSmallScreen.value = true;
+    } else {
+      isSmallScreen.value = false;
+    }
+  });
+
+  return {
+    isSmallScreen,
+  };
+}
