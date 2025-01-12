cedc7ce6452ee6faabe503b89bb3c40aff2aa796
Christian Valdez <christianvaldez36@hotmail.com>
2025-01-11 18:57:05 -0800

    added some more icons...

---
+++ twitter-ui/src/components/icons/Filters.vue
@@ -0,0 +1,9 @@
+<template>
+  <svg viewBox="0 0 24 24" aria-hidden="true">
+    <g>
+      <path
+        d="M14 6V3h2v8h-2V8H3V6h11zm7 2h-3.5V6H21v2zM8 16v-3h2v8H8v-3H3v-2h5zm13 2h-9.5v-2H21v2z"
+      ></path>
+    </g>
+  </svg>
+</template>
