524cefa9529f55b4dae74ad5427000bf2770b848
Christian Valdez <christianvaldez36@hotmail.com>
2025-03-07 18:04:18 -0800

    commented out cache options for comment and user_summary serializers in the mean time

--- twitter-api/app/serializers/user_summary_serializer.rb
+++ twitter-api/app/serializers/user_summary_serializer.rb
@@ -7,5 +7,5 @@ class UserSummarySerializer
 
   attributes :id, :name, :username, :profile_image_url
 
-  cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
+  # cache_options store: Rails.cache, namespace: 'jsonapi-serializer', expires_in: 1.hour
 end
