2efe8e752d982db8be7933e7115e5a021f8653c0
Christian Valdez <christianvaldez36@hotmail.com>
2025-04-13 00:12:41 -0700

    added method hanlders for chat requests

---
+++ twitter-ui/src/api/endpoints/messages.ts
@@ -0,0 +1,42 @@
+import type {
+  ChatMessageListResponse,
+  ChatThreadListResponse,
+  ChatThreadResponse,
+} from "@/types/ResponseTypes";
+import { authClient } from "../client";
+import type Thread from "@/models/Thread";
+import type { Message } from "@/models/Message";
+
+async function fetchChatThreads(): Promise<Thread[]> {
+  const res = await authClient.get<ChatThreadListResponse>("/threads");
+  return res.data.data.map((x) => x.attributes);
+}
+
+async function createChatThread(userIds: string[]): Promise<Thread> {
+  const res = await authClient.post<ChatThreadResponse>("/threads", {
+    userIds,
+  });
+  return res.data.data.attributes;
+}
+
+async function createMessage(threadId: string, body: string) {
+  authClient.post(
+    `/messages`,
+    { message: { body } },
+    { params: { chatThreadId: threadId } }
+  );
+}
+
+async function fetchMessages(threadId: string): Promise<Message[]> {
+  const res = await authClient.get<ChatMessageListResponse>("/messages", {
+    params: { chatThreadId: threadId },
+  });
+  return res.data.data
+    .map((x) => x.attributes)
+    .sort(
+      (a, b) =>
+        new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+    );
+}
+
+export { fetchChatThreads, createChatThread, fetchMessages, createMessage };
