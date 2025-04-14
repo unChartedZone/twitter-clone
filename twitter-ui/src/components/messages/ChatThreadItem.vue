b4d8044a908740b2b0c6cea9444b208dcefbdf6c
Christian Valdez <christianvaldez36@hotmail.com>
2025-04-13 18:37:26 -0700

    add ChatThreadItem component for displaying individual chat threads options

---
+++ twitter-ui/src/components/messages/ChatThreadItem.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import AvatarCircle from "../AvatarCircle.vue";
+import type Thread from "@/models/Thread";
+
+interface ChatThreadItemProps {
+  thread: Thread;
+}
+
+defineProps<ChatThreadItemProps>();
+</script>
+
+<template>
+  <li class="thread">
+    <div class="content" v-if="thread.users.length === 1">
+      <AvatarCircle :src="thread.users[0].profileImageUrl" />
+      <div>
+        <h4>{{ thread.users[0].name }}</h4>
+        <span>@</span>
+        <span>{{ thread.users[0].username }}</span>
+      </div>
+    </div>
+  </li>
+</template>
+
+<style scoped lang="scss">
+.thread {
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+
+  &:hover {
+    background-color: $gray-100;
+  }
+
+  .content {
+    display: flex;
+    gap: 0.75rem;
+  }
+}
+</style>
