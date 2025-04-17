60ef66525359a92d5752acf78154f077a7af3f2f
Christian Valdez <christianvaldez36@hotmail.com>
2025-04-17 09:51:39 -0700

    feat: add UserCard component for displaying user information

---
+++ twitter-ui/src/components/UserCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { UserSummary } from "@/models/User";
+import AvatarCircle from "./AvatarCircle.vue";
+
+defineProps<{ user: UserSummary }>();
+</script>
+
+<template>
+  <div class="user-card">
+    <AvatarCircle :src="user.profileImageUrl" />
+    <div>
+      <h4>{{ user.name }}</h4>
+      <span>@</span>
+      <span>{{ user.username }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.user-card {
+  display: flex;
+  gap: 0.75rem;
+}
+</style>
