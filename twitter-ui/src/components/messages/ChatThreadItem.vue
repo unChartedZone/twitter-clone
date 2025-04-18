32fa955f3dcb4fcef3e881e6807e1264ea437fad
Christian Valdez <christianvaldez36@hotmail.com>
2025-04-17 22:54:35 -0700

    refactor: replace inline user display with UserCard component for improved readability and maintainability

--- twitter-ui/src/components/messages/ChatThreadItem.vue
+++ twitter-ui/src/components/messages/ChatThreadItem.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import AvatarCircle from "../AvatarCircle.vue";
 import type Thread from "@/models/Thread";
+import ListItem from "../common/ListItem.vue";
+import UserCard from "../UserCard.vue";
 
 interface ChatThreadItemProps {
   thread: Thread;
@@ -10,30 +11,7 @@ defineProps<ChatThreadItemProps>();
 </script>
 
 <template>
-  <li class="thread">
-    <div class="content" v-if="thread.users.length === 1">
-      <AvatarCircle :src="thread.users[0].profileImageUrl" />
-      <div>
-        <h4>{{ thread.users[0].name }}</h4>
-        <span>@</span>
-        <span>{{ thread.users[0].username }}</span>
-      </div>
-    </div>
-  </li>
+  <ListItem>
+    <UserCard v-if="thread.users.length === 1" :user="thread.users[0]" />
+  </ListItem>
 </template>
-
-<style scoped lang="scss">
-.thread {
-  cursor: pointer;
-  padding: 0.5rem 1rem;
-
-  &:hover {
-    background-color: $gray-100;
-  }
-
-  .content {
-    display: flex;
-    gap: 0.75rem;
-  }
-}
-</style>
