ccbf12d32d2897f31810de30402fbc6f649c59cf
Christian Valdez <christianvaldez36@hotmail.com>
2025-11-02 17:52:04 -0800

    moved cors config to new api

--- twitter-api/config/initializers/cors.rb
+++ twitter-api/config/initializers/cors.rb
@@ -14,13 +14,3 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:8080', 'https://twitter.chrisvaldez.dev'
-
-    resource '*',
-             headers: :any,
-             methods: [:get, :post, :put, :patch, :delete, :options, :head],
-             credentials: true
-  end
-end
