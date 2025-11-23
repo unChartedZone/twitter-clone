bbb12776b6c90e4b4c9ca401c6e079181e3761bf
Christian Valdez <christianvaldez36@hotmail.com>
2025-11-22 21:21:14 -0800

    fix active storage attachment not using uuids

--- twitter-api-rails/db/migrate/20251022035633_create_active_storage_tables.active_storage.rb
+++ twitter-api-rails/db/migrate/20251022035633_create_active_storage_tables.active_storage.rb
@@ -50,8 +50,8 @@ class CreateActiveStorageTables < ActiveRecord::Migration[7.0]
     def primary_and_foreign_key_types
       config = Rails.configuration.generators
       setting = config.options[config.orm][:primary_key_type]
-      primary_key_type = setting || :primary_key
-      foreign_key_type = setting || :bigint
+      primary_key_type = setting || :uuid
+      foreign_key_type = setting || :uuid
       [ primary_key_type, foreign_key_type ]
     end
 end
