6a0bf65b5976ba818c2f9df5a795a729651951ac
Christian Valdez <christianvaldez36@hotmail.com>
2026-03-22 18:07:29 -0700

    remove some commented out code

--- twitter-ui/src/App.vue
+++ twitter-ui/src/App.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import { useAuthStore } from "@/stores/auth";
 import AppLoadingScreen from "./components/AppLoadingScreen.vue";
 import useAuth from "./lib/hooks/useAuth";
 
-const authStore = useAuthStore();
 const { refreshUserMutation } = useAuth();
 const { isPending } = refreshUserMutation;
 </script>
 
 <template>
-  <!-- <AppLoadingScreen v-if="authStore.userFetchStateLoading" /> -->
   <AppLoadingScreen v-if="isPending" />
   <component :is="$route.meta.layout || 'main'">
     <RouterView />
